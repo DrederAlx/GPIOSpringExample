e0f6eb9a071410d3e10006e9a1fcb2d42be7abe3
astogov <stogovandrei1985@gmail.com>
2020-12-30 13:39:22 +0300

    видео?

--- src/main/java/com/example/ledexample/controllers/VideoController.java
+++ src/main/java/com/example/ledexample/controllers/VideoController.java
@@ -13,6 +13,7 @@ public class VideoController {
         // ну и выполнить следующие команды (они включают камеру)
         //$sudo uv4l -nopreview --auto-video_nr --driver raspicam --encoding mjpeg --width 640 --height 480 --framerate 20 --server-option '--port=8081' --server-option '--max-queued-connections=30' --server-option '--max-streams=25' --server-option '--max-threads=29'
         //Обращаем внимание на порт!! и на ip в html
+        //&&
         return "video";
     }
 
